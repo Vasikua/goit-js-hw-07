5f4d731dd9466bf5c3aae8c3ffbc4c8d6b97fcb2
Vasyl Mandiak <Vasik_ua@ukr.net>
2024-01-20 21:40:58 -0600

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,5 @@
-function makeTransaction(quantity, pricePerDroid) {
-    let totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`; 
-}
+З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка
+елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
